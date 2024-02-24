cdbf0db850b27e1680fc8b30c4c9adf1fc5d04a1
lennart@familie-richter.de <lennart@familie-richter.de>
2024-02-24 15:10:24 -0800

    Finished Credentials, and beefed up About Me Page

--- application/frontend/src/components/Lennart.jsx
+++ application/frontend/src/components/Lennart.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function Lennart() {
     return (
+        <div>
         <h1>Lennart Richter</h1>
+        <p>Hey everyone, I'm Lennart a Senior here at SFSU. I'm excited to in this class and graduating this semester
+            I think building a full stack app will be a great experience, and I'm thrilled to be working with this team.
+        </p>
+        </div>
     )
 }
