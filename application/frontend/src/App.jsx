ea23dcc4e381e26813823ab87903838daee81b45
rf922 <rfabiani@sfsu.edu>
2024-02-24 16:07:26 -0800

    rework css to use tailwind and remove unused css files

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -6,7 +6,6 @@ import About from './components/about/About';
 import Profile from './components/profile/Profile';
 import { Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
-import Login from './components/login/Login';
 
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         <Route path="/about/:name" element={<Profile />} />
         <Route path="/about/" element={<About />} />
         
-        <Route path="/login/" element={<Login />} />
       </Routes>
     </div>
   );
