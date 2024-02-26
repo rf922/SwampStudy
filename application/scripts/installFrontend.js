b4e6b0751cff90541d8b5436ea9742e245151e73
lennart@familie-richter.de <lennart@familie-richter.de>
2024-02-26 15:19:24 -0800

    CI Fix

--- application/scripts/installFrontend.js
+++ application/scripts/installFrontend.js
@@ -1,7 +1,7 @@
 const {execSync} = require("child_process");
 const path = require("path");
 
-const DESTINATION = path.join(__dirname, "../frontend");
+const DESTINATION = path.resolve(path.join(__dirname, "../frontend"));
 
 const installFrontend = () => {
     execSync(`cd ${DESTINATION} && npm install`);
