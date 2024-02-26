ceb2a86d97a35d5f8865b789e44e5570922d9640
lennart@familie-richter.de <lennart@familie-richter.de>
2024-02-26 15:52:14 -0800

    CI Fix

--- application/scripts/build.js
+++ application/scripts/build.js
@@ -4,7 +4,12 @@ const path = require('path');
 const DESTINATION = path.resolve(path.join(__dirname, "../frontend"));
 
 const buildReactApp = () => {
-    const result = execSync(`cd ${DESTINATION} && npm run build`);
+    try{
+    const result = execSync(`cd ${DESTINATION} && npm run build2`);
     console.log(result.toString());
+    }
+    catch (e){
+        console.log(e.message)
+    }
 }
 buildReactApp();
