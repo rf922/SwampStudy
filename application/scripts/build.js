a7791a0d40da49a51b36fc48db4ccae5797bcef0
lennart@familie-richter.de <lennart@familie-richter.de>
2024-02-26 15:23:08 -0800

    CI Fix

--- application/scripts/build.js
+++ application/scripts/build.js
@@ -1,7 +1,7 @@
 const {execSync} = require('child_process');
 const path = require('path');
 
-const DESTINATION = path.join(__dirname, "../frontend");
+const DESTINATION = path.resolve(path.join(__dirname, "../frontend"));
 
 const buildReactApp = () => {
     execSync(`cd ${DESTINATION} && npm run build`);
--- application/scripts/installServer.js
+++ application/scripts/installServer.js
@@ -1,7 +1,7 @@
 const {execSync} = require("child_process");
 const path = require("path");
 
-const DESTINATION = path.join(__dirname, "../backend");
+const DESTINATION = path.resolve(path.join(__dirname, "../backend"));
 
 const installServer = () => {
     execSync(`cd ${DESTINATION} && npm install`);
