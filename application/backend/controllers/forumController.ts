425dd80c931b9d7cbede36da0c3b13176f53562f
Conrad Choi <choi.conrad@yahoo.com>
2024-03-18 11:44:53 -0700

    Current status of the forum controller
    Forum Controller.ts  stuck on the create question. It is returning to me missing meta data for the /account/.

--- application/backend/controllers/forumController.ts
+++ application/backend/controllers/forumController.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import { myDataSource } from "../app-data-source";
 import { Question } from "../entities/questions.entity";
 import { Answer } from "../entities/answer.entity";
+import { Account } from "../entities/account.entity";
 import { validate } from "class-validator";
+
+
 
 // Get questions
 export const getAllQuestions = async (req: Request, res: Response) => {
@@ -16,25 +19,41 @@ export const getAllQuestions = async (req: Request, res: Response) => {
   }
 };
 
-// Create question
+// Create question Testing in progress
+
 export const createQuestion = async (req: Request, res: Response) => {
   try {
-    const question = myDataSource.getRepository(Question).create(req.body);
-    const errors = await validate(question);
-    if (errors.length > 0) return res.status(400).json(errors);
+    // Extract the accountId and question from the request
+    const { accountId, question } = req.body;
+    // Parse the accountId to an integer this is a personal test to see if id was the issue
+    const Accountid = parseInt(accountId);
+    if(isNaN(Accountid)){return res.status(400).json({ message: "error in parse" });}
+    const questionText = question;
 
-    const savedQuestion = await myDataSource
-      .getRepository(Question)
-      .save(question);
+    // Check if the accountId exists in the database
+    const account = await myDataSource.getRepository(Account).findOneBy({ id: Accountid });
+    if (!account) {
+      return res.status(404).json({ message: "Account not found for the user" });
+    }
+
+    // Create a new question and associate it with the account
+    const newQuestion = new Question();
+    newQuestion.question = questionText;
+    // Validate the new question
+    const errors = await validate(newQuestion);
+    if (errors.length > 0) {
+      return res.status(400).json({message: "error in new question",details: errors});
+    }
+
+    // Save the new question to the database
+    const savedQuestion = await myDataSource.getRepository(Question).save(questionText);
     return res.status(201).json(savedQuestion);
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error creating question", details: error });
+    return res.status(500).json({ message: "Error creating question", details: error.toString() });
   }
 };
 
-// Get  answers to question
+// Get  answers to question in progress and untested
 export const getAnswersToQuestion = async (req: Request, res: Response) => {
   const questionId = parseInt(req.params.questionId);
   if (isNaN(questionId)) {
@@ -58,7 +77,7 @@ export const getAnswersToQuestion = async (req: Request, res: Response) => {
   }
 };
 
-// Create new answer to question
+// Create new answer to question in progress and untested
 export const createAnswer = async (req: Request, res: Response) => {
   try {
     const answer = myDataSource.getRepository(Answer).create(req.body);
