b41628b70b71c475e99041b851efc4870ba134d1
Edmund Huang <ehuang6@mail.sfsu.edu>
2024-04-01 13:33:30 -0700

    Fixed typo in matching.jsx

--- application/frontend/src/components/matching/Matching.jsx
+++ application/frontend/src/components/matching/Matching.jsx
@@ -1,7 +1,7 @@
 export const Matching = () => {
   return (
     <div>
-      <p>Swamp Study&apos;s Matching section is Comming soon !</p>
+      <p>Swamp Study&apos;s Matching section is coming soon!</p>
     </div>
   );
 };
