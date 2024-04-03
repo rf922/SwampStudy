2cb19e97e5360ff88bf7ab78194bce9dcf2aa3ce
Conrad Choi <choi.conrad@yahoo.com>
2024-04-03 16:23:29 -0700

    authenticated the routers
    Authenticated the userRouter, accountRouter,forumRouter,matchRouter, and likeRouter.

--- application/backend/routers/forumRouter.ts
+++ application/backend/routers/forumRouter.ts
@@ -16,7 +16,7 @@ const forumRouter = Router();
 
 // Routes for questions
 forumRouter.get("/questions", getAllQuestions);
-forumRouter.get("/questions/:questionId", getQuestion);
+forumRouter.get("/questions/:questionId", isAuthenticated,getQuestion);
 forumRouter.post("/question", isAuthenticated, createPost);
 
 // Routes for answers
@@ -28,10 +28,10 @@ forumRouter.post(
 );
 
 // Route for getting all classes
-forumRouter.get("/classes", getClasses);
+forumRouter.get("/classes", isAuthenticated,getClasses);
 
-forumRouter.get("/departments/listing", getClassesByDepartment);
+forumRouter.get("/departments/listing", isAuthenticated,getClassesByDepartment);
 
-forumRouter.get("/departments/threads", getThreadsByDepartment);
+forumRouter.get("/departments/threads", isAuthenticated,getThreadsByDepartment);
 
 export default forumRouter;
--- application/backend/routers/likeRouter.ts
+++ application/backend/routers/likeRouter.ts
@@ -1,8 +1,9 @@
 import { postLike } from "../controllers/likeController";
+import { isAuthenticated } from "./../middleware/isAuthenticated";
 import express from "express";
 
 const likeRouter = express.Router();
 
-likeRouter.post("/", postLike);
+likeRouter.post("/",isAuthenticated ,postLike);
 
 export default likeRouter;
