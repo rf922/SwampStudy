9de08ee8e6e893c45f3765b85e1e41d24d90ecba
rf922 <rfabiani@sfsu.edu>
2024-04-04 12:07:55 -0700

    remove middleware from get questions rote

--- application/backend/routers/forumRouter.ts
+++ application/backend/routers/forumRouter.ts
@@ -29,12 +29,12 @@ forumRouter.post(
 );
 
 // Route for getting all classes
-forumRouter.get("/classes", isAuthenticated,getClasses);
+forumRouter.get("/classes", getClasses);
 
-forumRouter.get("/departments/listing", isAuthenticated,getClassesByDepartment);
+forumRouter.get("/departments/listing", getClassesByDepartment);
 
-forumRouter.get("/departments/threads", isAuthenticated, getThreadsByDepartment);
+forumRouter.get("/departments/threads", getThreadsByDepartment);
 
-forumRouter.get("/threads/search", isAuthenticated,threadSearch);
+forumRouter.get("/threads/search", threadSearch);
 
 export default forumRouter;
