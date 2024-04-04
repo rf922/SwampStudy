2bb87cf30c570f77ba7ee562492cc4b1998c2b0d
lennart@familie-richter.de <lennart@familie-richter.de>
2024-04-04 12:28:10 -0700

    Added the Rating Entity and Controller

---
+++ application/backend/routers/ratingRouter.ts
@@ -0,0 +1,10 @@
+import { postRating, getRating } from "../controllers/ratingController";
+import { isAuthenticated } from "./../middleware/isAuthenticated";
+import express from "express";
+
+const ratingrouter = express.Router();
+
+ratingrouter.post("/", isAuthenticated, postRating);
+ratingrouter.get("/", isAuthenticated, getRating);
+
+export default ratingrouter;
